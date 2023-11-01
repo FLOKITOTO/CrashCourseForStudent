1223b9e5832ee3b1642d595b54dca7deed00a7fa
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2023-11-01 13:20:27 +0100

    data

--- src/app/main/all-products/all-products.component.ts
+++ src/app/main/all-products/all-products.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { PRODUCTS } from 'src/app/_commons/datas';
+import { Product } from 'src/app/_commons/models/product';
 
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
-  styleUrls: ['./all-products.component.scss']
+  styleUrls: ['./all-products.component.scss'],
 })
 export class AllProductsComponent implements OnInit {
+  public products?: Product[] = PRODUCTS;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
